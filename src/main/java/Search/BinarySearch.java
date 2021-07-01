8fabe7c18041ed4df80fdb70c1abf5fcdd4dd807
klniu <hughgao01@gmail.com>
2021-07-01 17:07:39 +0800

    优化算法

--- src/main/java/Search/BinarySearch.java
+++ src/main/java/Search/BinarySearch.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class BinarySearch {
     /**
      * 二分法查找元素。要求提供的数组是已经排序完成的。
-     *
+     * 参考《算法图解》：1.2.1
      * @param arr  数组
      * @param item 需要查找的元素
      * @return 元素的位置，如果未找到，返回-1
@@ -15,62 +15,25 @@ public class BinarySearch {
     public static <T extends Comparable<T>> int search(@NonNull List<T> arr, @NonNull T item) {
         if (arr.size() == 0) {
             return -1;
-        } else if (arr.size() == 1) {
-            return arr.get(0).equals(item) ? 0 : -1;
         }
+        int low = 0;
+        int high = arr.size() - 1;
 
-        int middle = arr.size() / 2;
-
-        T ele = arr.get(middle);
-        if (ele == null) {
-            throw new IllegalArgumentException("列表元素不能为空，元素索引：" + arr.size() / 2);
-        }
-
-        int compareResult = item.compareTo(ele);
-        if (compareResult == 0) {
-            return middle;
-        } else if (compareResult > 0) {
-            int biggerResult = search(arr.subList(middle, arr.size()), item);
-            if (biggerResult == -1) return -1;
-            return middle + biggerResult;
-        } else {
-            return search(arr.subList(0, middle), item);
-        }
-    }
-
-    /**
-     * 二分法查找元素。要求提供的数组是已经排序完成的。
-     *
-     * @param arr  数组
-     * @param item 需要查找的元素
-     * @return 元素的位置，如果未找到，返回-1
-     */
-    public static <T extends Comparable<T>> int searchLoop(@NonNull List<T> arr, @NonNull T item) {
-        if (arr.size() == 0) {
-            return -1;
-        }
-
-        int startIndex = 0;
-        int endIndex = arr.size();
-        while (true) {
-            int middle = (startIndex + endIndex) / 2;
+        while (low <= high) {
+            int middle = (low + high) / 2;
             T ele = arr.get(middle);
             if (ele == null) {
                 throw new IllegalArgumentException("列表元素不能为空，元素索引：" + arr.size() / 2);
             }
-
             int compareResult = item.compareTo(ele);
             if (compareResult == 0) {
                 return middle;
-            } else if (startIndex == middle || endIndex == middle) {
-                // 已经穷尽查找
-                return -1;
             } else if (compareResult > 0) {
-                startIndex = middle;
+                low = middle + 1;
             } else {
-                endIndex = middle;
+                high = middle - 1;
             }
         }
-
+        return -1;
     }
 }
--- src/test/java/Search/BinarySearchTest.java
+++ src/test/java/Search/BinarySearchTest.java
@@ -2,7 +2,6 @@ package Search;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -31,27 +30,6 @@ class BinarySearchTest {
         LocalDateTime before = LocalDateTime.now();
         System.out.println("开始搜索" + before);
         assertEquals(49999999, BinarySearch.search(list, 49999999));
-        System.out.println(Duration.between(before, LocalDateTime.now()).getNano());
-        System.out.println("结束搜索" + LocalDateTime.now());
-    }
-
-    @Test
-    public void searchLoopTest() {
-        // empty array
-        assertEquals(-1, BinarySearch.searchLoop(new ArrayList<>(), 4));
-        // not found
-        assertEquals(-1, BinarySearch.searchLoop(Arrays.asList(1, 2, 3), 4));
-        // odd
-        assertEquals(3, BinarySearch.searchLoop(Arrays.asList(1, 2, 3, 7, 9), 7));
-        // even
-        assertEquals(2, BinarySearch.searchLoop(Arrays.asList(1, 2, 7, 9), 7));
-        assertEquals(-1, BinarySearch.searchLoop(Arrays.asList(1, 2, 3, 5), 4));
-
-        // 性能测试
-        List<Integer> list = IntStream.range(0, 100000000).boxed().collect(Collectors.toList());
-        LocalDateTime before = LocalDateTime.now();
-        System.out.println("开始搜索" + before);
-        assertEquals(49999999, BinarySearch.searchLoop(list, 49999999));
         System.out.println(Duration.between(before, LocalDateTime.now()).getNano());
         System.out.println("结束搜索" + LocalDateTime.now());
     }
